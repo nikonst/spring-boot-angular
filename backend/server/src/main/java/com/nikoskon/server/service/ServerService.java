44ec54a5ad28f135f94448a0fdbec9bc295c3c33
nikonst <nikonst@otenet.gr>
2023-07-25 10:11:30 +0300

    Work on Server Service

--- backend/server/src/main/java/com/nikoskon/server/service/ServerService.java
+++ backend/server/src/main/java/com/nikoskon/server/service/ServerService.java
@@ -2,11 +2,12 @@ package com.nikoskon.server.service;
 
 import com.nikoskon.server.model.Server;
 
+import java.io.IOException;
 import java.util.Collection;
 
 public interface ServerService {
     Server create(Server server);
-    Server ping(String ipAddress);
+    Server ping(String ipAddress) throws IOException;
     Collection<Server> list(int limit);
     Server get(long id);
     Server update(Server server);
---
+++ backend/server/src/main/java/com/nikoskon/server/service/implementation/ServerServiceImpl.java
@@ -0,0 +1,70 @@
+package com.nikoskon.server.service.implementation;
+
+import com.nikoskon.server.enumeration.Status;
+import com.nikoskon.server.model.Server;
+import com.nikoskon.server.model.repo.ServerRepo;
+import com.nikoskon.server.service.ServerService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.Collection;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+@Slf4j
+public class ServerServiceImpl implements ServerService {
+    private final ServerRepo serverRepo;
+    @Override
+    public Server create(Server server) {
+        log.info("Saving server... ", server.getName());
+        server.setImageUrl(setServerImageUrl());
+        return serverRepo.save(server);
+    }
+
+    private String setServerImageUrl() {
+        String[] imgNames = {"server1.png", "server2.png", "server3.png", "server4.png"};
+        return null;
+    }
+
+    @Override
+    public Server ping(String ipAddress) throws IOException {
+        log.info("Pinging server IP... ", ipAddress);
+        Server server = serverRepo.findByIPAddress(ipAddress);
+        InetAddress inetAddress = InetAddress.getByName(ipAddress);
+        server.setStatus(inetAddress.isReachable(10000) ? Status.SERVER_UP : Status.SERVER_DOWN);
+        serverRepo.save(server);
+        return server;
+    }
+
+    @Override
+    public Collection<Server> list(int limit) {
+        log.info("Fetching all servers... ");
+        return serverRepo.findAll(PageRequest.of(0, limit)).toList();
+    }
+
+    @Override
+    public Server get(long id) {
+        log.info("Fetching Servers Id... ", id);
+        return serverRepo.findById(id).get();
+    }
+
+    @Override
+    public Server update(Server server) {
+        log.info("Updating server... ", server.getName());
+        return serverRepo.save(server);
+    }
+
+    @Override
+    public Boolean delete(long id) {
+
+        log.info("Deleting server... ",id);
+        serverRepo.deleteById(id);
+        return Boolean.TRUE;
+    }
+}
