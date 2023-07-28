e661813734e9902914218a585a41af5e753c07c5
nikonst <nikonst@otenet.gr>
2023-07-28 11:45:40 +0300

    Work on Spring DB - Server Application

--- backend/server/src/main/java/com/nikoskon/server/ServerApplication.java
+++ backend/server/src/main/java/com/nikoskon/server/ServerApplication.java
@@ -1,7 +1,12 @@
 package com.nikoskon.server;
 
+import com.nikoskon.server.enumeration.Status;
+import com.nikoskon.server.model.Server;
+import com.nikoskon.server.model.repo.ServerRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -9,5 +14,25 @@ public class ServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
-
+	@Bean
+	CommandLineRunner run(ServerRepo serverRepo) {
+		return args -> {
+			serverRepo.save(new Server(null, "192.168.116.2",
+					"Ubuntu Linux", "16 GB", "PC",
+					"htp://localhost:8080/server/image/server1.png",
+					Status.SERVER_UP));
+			serverRepo.save(new Server(null, "192.168.116.25",
+					"Fedora Linux", "16 GB", "PC",
+					"htp://localhost:8080/server/image/server2.png",
+					Status.SERVER_UP));
+			serverRepo.save(new Server(null, "192.168.116.29",
+					"Red Hat Linux", "16 GB", "PC",
+					"htp://localhost:8080/server/image/server3.png",
+					Status.SERVER_UP));
+			serverRepo.save(new Server(null, "192.168.116.33",
+					"Zorin Linux", "16 GB", "PC",
+					"htp://localhost:8080/server/image/server4.png",
+					Status.SERVER_UP));
+		};
+	}
 }
