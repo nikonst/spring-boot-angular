10f1c9c69c4f50ec84912371d0b14b7290f25b27
nikonst <nikonst@otenet.gr>
2023-07-28 11:35:18 +0300

    Finishing ServerResource controller

--- backend/server/src/main/java/com/nikoskon/server/resource/ServerResource.java
+++ backend/server/src/main/java/com/nikoskon/server/resource/ServerResource.java
@@ -4,20 +4,22 @@ import com.nikoskon.server.enumeration.Status;
 import com.nikoskon.server.model.Response;
 import com.nikoskon.server.model.Server;
 import com.nikoskon.server.service.implementation.ServerServiceImpl;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.Map;
 
 import static java.util.Optional.of;
+import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.MediaType.IMAGE_PNG_VALUE;
 
 @RestController
 @RequestMapping("/server")
@@ -32,7 +34,8 @@ public class ServerResource {
                         data(Map.of("servers", serverService.list(30))).
                         message("Servers retrieved").
                         status(OK).
-                        statusCode(OK.value()).build()
+                        statusCode(OK.value()).
+                        build()
 
         );
     }
@@ -42,11 +45,53 @@ public class ServerResource {
         Server server = serverService.ping(ipAddress);
         return ResponseEntity.ok(
                 Response.builder().timeStamp(LocalDateTime.now()).
-                        data(Map.of("server", server).
+                        data(Map.of("server", server)).
                         message(server.getStatus() == Status.SERVER_UP ? "Ping Success" : "Ping Fail").
                         status(OK).
-                        statusCode(OK.value()).build()
-
+                        statusCode(OK.value()).
+                        build()
         );
     }
+
+    @PostMapping("/save")
+    public ResponseEntity<Response> saveServer(@RequestBody @Valid Server server) throws IOException {
+        return ResponseEntity.ok(
+                Response.builder().timeStamp(LocalDateTime.now()).
+                        data(Map.of("server", serverService.create(server))).
+                        message("Server Created").
+                        status(CREATED).
+                        statusCode(CREATED.value()).
+                        build()
+        );
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Response> getServer(@PathVariable("id") Long id) throws IOException {
+        return ResponseEntity.ok(
+                Response.builder().timeStamp(LocalDateTime.now()).
+                        data(Map.of("server", serverService.get(id))).
+                        message("Server retrieved").
+                        status(OK).
+                        statusCode(OK.value()).
+                        build()
+        );
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Response> deleteServer(@PathVariable("id") Long id) throws IOException {
+        return ResponseEntity.ok(
+                Response.builder().timeStamp(LocalDateTime.now()).
+                        data(Map.of("server", serverService.delete(id))).
+                        message("Server deleted").
+                        status(OK).
+                        statusCode(OK.value()).
+                        build()
+        );
+    }
+
+    @GetMapping(path="/image/{fileName}", produces = IMAGE_PNG_VALUE)
+    public byte[] getServerImage(@PathVariable("fileName") String fileName) throws IOException {
+        return Files.readAllBytes(Paths.get(System.getProperty("user.home" + "Downloads/images/" + fileName)));
+    }
+
 }
