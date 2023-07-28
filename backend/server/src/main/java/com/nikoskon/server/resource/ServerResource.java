2f26ba34a739465b6579a18f01d9e5e9a7b94774
nikonst <nikonst@otenet.gr>
2023-07-28 10:04:34 +0300

    Work on ServerResource

--- backend/server/src/main/java/com/nikoskon/server/resource/ServerResource.java
+++ backend/server/src/main/java/com/nikoskon/server/resource/ServerResource.java
@@ -1,17 +1,22 @@
 package com.nikoskon.server.resource;
 
+import com.nikoskon.server.enumeration.Status;
 import com.nikoskon.server.model.Response;
+import com.nikoskon.server.model.Server;
 import com.nikoskon.server.service.implementation.ServerServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Map;
 
+import static java.util.Optional.of;
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
@@ -26,6 +31,19 @@ public class ServerResource {
                 Response.builder().timeStamp(LocalDateTime.now()).
                         data(Map.of("servers", serverService.list(30))).
                         message("Servers retrieved").
+                        status(OK).
+                        statusCode(OK.value()).build()
+
+        );
+    }
+
+    @GetMapping("/ping/{ipAddress}")
+    public ResponseEntity<Response> pingServer(@PathVariable("ipAddress") String ipAddress) throws IOException {
+        Server server = serverService.ping(ipAddress);
+        return ResponseEntity.ok(
+                Response.builder().timeStamp(LocalDateTime.now()).
+                        data(Map.of("server", server).
+                        message(server.getStatus() == Status.SERVER_UP ? "Ping Success" : "Ping Fail").
                         status(OK).
                         statusCode(OK.value()).build()
 
