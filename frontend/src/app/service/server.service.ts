6adbd7f7b6af523375e3dcf71bb2480badbb5523
nikonst <nikonst@otenet.gr>
2023-08-04 10:27:45 +0300

    Work on frontend server service

--- frontend/src/app/service/server.service.ts
+++ frontend/src/app/service/server.service.ts
@@ -1,25 +1,73 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CustomResponse } from '../interface/custom-response';
-import { Observable, catchError, tap } from 'rxjs';
+import { Observable, catchError, tap, throwError } from 'rxjs';
+import { Server } from '../interface/server';
+import { Status } from '../enum/status.enum';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class ServerService {
-  handelError(handelError: any): Observable<never> {
-    throw new Error('Method not implemented.');
+  private handelError(error: HttpErrorResponse): Observable<never> {
+    console.log(error)
+    return throwError(`An error occured - Error code: ${error.status}`)
   }
 
   private readonly apiUrl = 'any';
 
   constructor(private http: HttpClient) { }
 
-  server$ = <Observable<CustomResponse>>
-  this.http.get<CustomResponse>(`${this.apiUrl}/server/list`)
-  .pipe(
-    tap(console.log),
-    catchError(this.handelError)
-  )
+  servers$ = <Observable<CustomResponse>>
+    this.http.get<CustomResponse>(`${this.apiUrl}/server/list`)
+      .pipe(
+        tap(console.log),
+        catchError(this.handelError)
+      )
+
+  save$ = (server: Server) => <Observable<CustomResponse>>
+    this.http.post<CustomResponse>(`${this.apiUrl}/server/list`, server)
+      .pipe(
+        tap(console.log),
+        catchError(this.handelError)
+      )
+
+  ping$ = (ipAddress: string) => <Observable<CustomResponse>>
+    this.http.get<CustomResponse>(`${this.apiUrl}/server/ping'${ipAddress}`)
+      .pipe(
+        tap(console.log),
+        catchError(this.handelError)
+      )
+
+  delete$ = (serverId: number) => <Observable<CustomResponse>>
+    this.http.delete<CustomResponse>(`${this.apiUrl}/server/delete'${serverId}`)
+      .pipe(
+        tap(console.log),
+        catchError(this.handelError)
+      )
+
+  filter$ = (status: Status, response: CustomResponse) => <Observable<CustomResponse>>
+    new Observable<CustomResponse>(
+      subscriber => {
+        console.log(response)
+        subscriber.next(
+          status === 'ALL' ? {
+            ...response, message: `Servers filtered by ${status} status`
+          } : {
+            ...response, message: response.data.servers.filter(server =>
+              server.status === status).length > 0
+              ?
+              `Servers filtered by ${status === Status.SERVER_UP
+                ? 'SERVER UP' : 'SERVER DOWN'} status`
+              : `No servers of ${status} found`,
+            data: {
+              servers: response.data.servers.filter(server =>
+                server.status === status)
+            }
+          }
+        )
+        subscriber.complete()
+      }
+    )
 }
