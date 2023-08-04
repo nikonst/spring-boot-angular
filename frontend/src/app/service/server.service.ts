370770e4b78ad7dcc7c39c41fa59c7b552b6b0e2
nikonst <nikonst@otenet.gr>
2023-08-04 08:35:25 +0300

    Work on server service ts

--- frontend/src/app/service/server.service.ts
+++ frontend/src/app/service/server.service.ts
@@ -6,16 +6,18 @@ import { Observable, catchError, tap } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
+
 export class ServerService {
   handelError(handelError: any): Observable<never> {
     throw new Error('Method not implemented.');
   }
-  
+
   private readonly apiUrl = 'any';
 
   constructor(private http: HttpClient) { }
 
-  server$ = this.http.get<CustomResponse>(`${this.apiUrl}/server/list`)
+  server$ = <Observable<CustomResponse>>
+  this.http.get<CustomResponse>(`${this.apiUrl}/server/list`)
   .pipe(
     tap(console.log),
     catchError(this.handelError)
