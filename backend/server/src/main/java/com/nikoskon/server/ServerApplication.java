d9d267eb0b4dc282d22994013b5929cc4c43cf9e
nikonst <nikonst@otenet.gr>
2023-08-14 09:59:47 +0300

    Add cors config at backend

--- backend/server/src/main/java/com/nikoskon/server/ServerApplication.java
+++ backend/server/src/main/java/com/nikoskon/server/ServerApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -19,20 +24,32 @@ public class ServerApplication {
 		return args -> {
 			serverRepo.save(new Server(null, "192.168.116.2",
 					"Ubuntu Linux", "16 GB", "PC",
-					"htp://localhost:8080/server/image/server1.png",
+					"http://localhost:8080/server/image/server1.png",
 					Status.SERVER_UP));
 			serverRepo.save(new Server(null, "192.168.116.25",
 					"Fedora Linux", "16 GB", "PC",
-					"htp://localhost:8080/server/image/server2.png",
+					"http://localhost:8080/server/image/server2.png",
 					Status.SERVER_UP));
 			serverRepo.save(new Server(null, "192.168.116.29",
 					"Red Hat Linux", "16 GB", "PC",
-					"htp://localhost:8080/server/image/server3.png",
+					"http://localhost:8080/server/image/server3.png",
 					Status.SERVER_UP));
 			serverRepo.save(new Server(null, "192.168.116.33",
 					"Zorin Linux", "16 GB", "PC",
-					"htp://localhost:8080/server/image/server4.png",
+					"http://localhost:8080/server/image/server4.png",
 					Status.SERVER_UP));
 		};
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+		configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+		configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }
